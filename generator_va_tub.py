e9182a1b100b46c8fca1a30703f955fa83cf22cc
nice6879 <elmurod195195@gamil.com>
2024-05-30 18:17:18 +0500

    generatorlar

---
+++ generator_va_tub.py
@@ -0,0 +1,71 @@
+# def f(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, int(n**0.5) + 1):
+#         if n % i == 0:
+#             return False
+#     return True
+
+# x = [num for num in range(1, 101) if f(num)]
+
+# print(x)
+
+
+
+
+
+# def f(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
+
+# for num in range(1, 101):
+#     if f(num):
+#         print(num)
+
+
+
+
+# number = 10
+# while number < 10:
+#     print(number)
+#     number +=1
+# else:
+#     print('dastur tohtadi')
+
+# number = 0
+
+
+# while True:
+#     print(number)
+#     number +=1
+#     if number >=10:
+#         break
+
+# else:
+#     print('dastur tohtadi')
+
+
+def generate_numbers(num1):
+    count = 0
+    returns = 0 
+    while returns<num1:
+        count +=1
+        if count%2 ==0:
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
